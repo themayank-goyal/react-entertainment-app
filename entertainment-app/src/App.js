d7d43e2cbc86ae1a02fdafabf5a62059d571c8e3
Mayank Goyal <mayankg1908@gmail.com>
2022-05-12 15:20:29 +0530

    search functionality done

--- entertainment-app/src/App.js
+++ entertainment-app/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [searchString, setSearchString] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [filterMovies, setFilterMovies] = useState(movies);
+
+  useEffect(() => {
+    setMovies(data);
+  }, []);
+
+  useEffect(() => {
+    const newfilterMovies = movies.filter((mv) => {
+      return mv.title.toLocaleLowerCase().includes(searchString);
+    });
+    setFilterMovies(newfilterMovies);
+  }, [movies, searchString]);
+
+  const handleSearch = (e) => {
+    const newSearchString = e.target.value.toLocaleLowerCase();
+    setSearchString(newSearchString);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <nav className='navbar'>
+        <div className='nav-link'>TV</div>
+        <div className='nav-link'>Movies</div>
+        <div className='nav-link'>bookmark</div>
+        <div className='nav-link'>TV</div>
+      </nav> */}
+      <input type='search' onChange={handleSearch} />
+      <div className='section-movies'>
+        {filterMovies.map((mv) => {
+          return (
+            <div className='movie-container'>
+              <div>
+                <img
+                  className='movie-img'
+                  src={mv.thumbnail.regular.medium}
+                  alt=''
+                />
+              </div>
+              <ul className='mov-details'>
+                <li>{mv.year}</li>
+                <li>{mv.category}</li>
+                <li>{mv.rating}</li>
+              </ul>
+              <span>{mv.title}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
